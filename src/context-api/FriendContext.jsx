e307c400bb938ac69c22b9ef48e3b5ec780141ae
Ruhul Amin <mdruhulamins786@gmail.com>
2026-04-13 23:42:29 +0600

    create context-api component and testing data show

---
+++ src/context-api/FriendContext.jsx
@@ -0,0 +1,29 @@
+import { createContext, useEffect, useState } from "react";
+
+export const FriendContextApi = createContext();
+
+const FriendContext = ({ children }) => {
+  const [friends, setFriends] = useState([]);
+
+  useEffect(() => {
+    const loadData = async () => {
+      try {
+        const res = await fetch("/friends.json");
+        const data = await res.json();
+        setFriends(data);
+      } catch (error) {
+        console.error("Failed to load friends:", error);
+      }
+    };
+
+    loadData();
+  }, []);
+
+  return (
+    <FriendContextApi.Provider value={{ friends, setFriends }}>
+      {children}
+    </FriendContextApi.Provider>
+  );
+};
+
+export default FriendContext;
